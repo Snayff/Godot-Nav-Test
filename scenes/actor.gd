2a3fa2a73c1b36663bc9e07220c643e43ca4b00e
Snayff <snaith2@gmail.com>
2024-05-16 13:15:56 +0100

    added states, animation

--- scenes/actor.gd
+++ scenes/actor.gd
@@ -1,26 +1,37 @@
 extends CharacterBody2D
 
+@onready var nav_agent: NavigationAgent2D = $NavigationAgent2D
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 const movement_speed: float = 80.0
-@onready var nav_agent: NavigationAgent2D = $NavigationAgent2D
+enum STATE {
+	MOVING,
+	IDLE
+}
+
 var target_destination: Vector2
+var current_state: int = STATE.IDLE
 
 func _ready() -> void:
 	EventBus.connect("destination_set", set_destination)
 	nav_agent.velocity_computed.connect(Callable(_on_velocity_computed))
 
+	set_state(STATE.IDLE)
+
+func _process(delta: float) -> void:
+	if nav_agent.is_navigation_finished():
+		set_state(STATE.IDLE)
+	else:
+		set_state(STATE.MOVING)
 
 func _physics_process(delta) -> void:
-	if nav_agent.is_navigation_finished():
-		return
-		
-	var next_path_pos: Vector2 = nav_agent.get_next_path_position()
-	var new_velocity: Vector2 = global_position.direction_to(next_path_pos) * movement_speed
-	if nav_agent.avoidance_enabled:
-		nav_agent.set_velocity(new_velocity)
-	else:
-		_on_velocity_computed(new_velocity)
-
+	if current_state == STATE.MOVING:	
+		var next_path_pos: Vector2 = nav_agent.get_next_path_position()
+		var new_velocity: Vector2 = global_position.direction_to(next_path_pos) * movement_speed
+		if nav_agent.avoidance_enabled:
+			nav_agent.set_velocity(new_velocity)
+		else:
+			_on_velocity_computed(new_velocity)
 
 func set_destination(destination: Vector2) -> void:
 	target_destination = destination
@@ -36,3 +47,21 @@ func _on_velocity_computed(safe_velocity: Vector2):
 func _on_timer_path_timeout() -> void:
 	# TODO: change to queing for path update, to limit fps drop
 	_update_path()
+	
+func set_state(new_state: STATE) -> void:
+	_exit_state()
+	_enter_state(new_state)
+	
+	current_state = new_state
+	
+func _exit_state() -> void:
+	if current_state == STATE.MOVING:
+		pass
+	elif current_state == STATE.IDLE:
+		pass
+
+func _enter_state(state: STATE) -> void:
+	if state == STATE.MOVING:
+		sprite.play("moving")
+	elif state == STATE.IDLE:
+		sprite.play("idle")
